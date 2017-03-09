17bf09052e6d62b88178e32c0137ce7605d05d91
OBSIDIAN\james <james.o.herd@gmail.com>
2017-03-09 16:56:16 +1000

    Add project files.

---
+++ JimsSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JimsSite.MvcApplication" Language="C#" %>
---
+++ JimsSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JimsSite.Startup))]
+namespace JimsSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
